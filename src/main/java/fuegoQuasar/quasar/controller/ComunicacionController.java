bbaca5c3991c3ac0ce49734a3eb9fd507b554d6d
John Doe <johndoe@example.com>
2020-11-30 19:12:36 -0300

    controler y clases entities

---
+++ src/main/java/fuegoQuasar/quasar/controller/ComunicacionController.java
@@ -0,0 +1,96 @@
+package fuegoQuasar.quasar.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import fuegoQuasar.quasar.excepciones.BadRequestException;
+import fuegoQuasar.quasar.excepciones.MensajeException;
+import fuegoQuasar.quasar.excepciones.MensajeInterceptadoException;
+import fuegoQuasar.quasar.excepciones.UbicacionException;
+import fuegoQuasar.quasar.model.entities.MensajeInterceptado;
+import fuegoQuasar.quasar.model.entities.MensajeWrapper;
+import fuegoQuasar.quasar.model.response.MensajeResponse;
+import fuegoQuasar.quasar.services.ComunicacionService;
+
+@RestController
+public class ComunicacionController {
+
+
+	
+	@Autowired
+	ComunicacionService comunicacionService;
+	
+	
+	
+	@PostMapping(path = "/topsecret", consumes = {MediaType.APPLICATION_JSON_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
+	public ResponseEntity topSecret(RequestEntity<MensajeWrapper> requestEntity) {
+		
+		if( requestEntity ==  null || requestEntity.getBody() == null || requestEntity.getBody().getSatelites() == null 
+				|| requestEntity.getBody().getSatelites().size() != 3 ) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new MensajeResponse("El request es invalido"));
+		}
+		
+		try {	
+			//comunicacionService.getTopSecretResponse(requestEntity.getBody().getSatelites());
+			return ResponseEntity.status(HttpStatus.OK).body(comunicacionService.getTopSecretResponse(requestEntity.getBody().getSatelites()));
+		
+		} catch (UbicacionException e) {	
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new MensajeResponse(e.getMessage()));
+			
+		} catch (MensajeException e){
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new MensajeResponse(e.getMessage()));
+        
+		} catch (BadRequestException e){
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new MensajeResponse(e.getMessage()));
+        }
+
+	}
+	/*
+	 * curl -X POST localhost:8080/employees -H 'Content-type:application/json' -d '{"name": "Samwise Gamgee", "role": "gardener"}'
+	 * 
+	 * {"satellites": [ { "name": "kenobi", "distance": 5.0, "message": ["este", "", "", "mensaje", ""]	}, {"name": "skywalker", "distance": 5.0, "message": ["", "es", "", "", "secreto"]}, {"name": "sato", "distance": 13.0, "message": ["", "", "un", "", ""] } ] }
+	 */
+	
+	@GetMapping(path = "/topsecret_split", produces = {MediaType.APPLICATION_JSON_VALUE})
+	public ResponseEntity topSecretSplit() {
+		
+		try {	
+			
+			return ResponseEntity.status(HttpStatus.OK).body(comunicacionService.getTopSecretSplitResponse());
+		
+		} catch (UbicacionException e) {	
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new MensajeResponse(e.getMessage()));
+			
+		} catch (MensajeException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new MensajeResponse(e.getMessage()));
+        
+		} catch (MensajeInterceptadoException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new MensajeResponse("No hay suficiente informacion."));
+            
+        } catch (BadRequestException e){
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new MensajeResponse(e.getMessage()));
+        }
+	}
+	
+	
+	@PostMapping(path = "/topsecret_split/{name}", consumes = {MediaType.APPLICATION_JSON_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
+	public ResponseEntity one(@PathVariable String name, RequestEntity<MensajeInterceptado> requestEntity) {
+		try {	
+			
+			return ResponseEntity.status(HttpStatus.OK).body(comunicacionService.insertTopSecretRequest(name, requestEntity.getBody()));
+		
+		} catch (BadRequestException e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new MensajeResponse(e.getMessage()));
+        
+		} 
+		
+	}
+	
+}
---
+++ src/main/java/fuegoQuasar/quasar/model/entities/MensajeInterceptado.java
@@ -0,0 +1,111 @@
+package fuegoQuasar.quasar.model.entities;
+
+import java.util.Arrays;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class MensajeInterceptado {
+	
+	
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String name;
+	private Double distance;
+	private String[] message;
+	
+
+	public MensajeInterceptado() {
+	}
+	
+	
+	public MensajeInterceptado(String name, Double distance, String[] message) {
+		super();
+		this.name = name;
+		this.distance = distance;
+		this.message = message;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public Double getDistance() {
+		return distance;
+	}
+	
+	public void setDistance(Double distance) {
+		this.distance = distance;
+	}
+	
+	public String[] getMessage() {
+		return message;
+	}
+	
+	public void setMessage(String[] message) {
+		this.message = message;
+	}
+	
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((distance == null) ? 0 : distance.hashCode());
+		result = prime * result + Arrays.hashCode(message);
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		MensajeInterceptado other = (MensajeInterceptado) obj;
+		if (distance == null) {
+			if (other.distance != null)
+				return false;
+		} else if (!distance.equals(other.distance))
+			return false;
+		if (!Arrays.equals(message, other.message))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "MensajeInterceptado [name=" + name + ", distance=" + distance + ", message="
+				+ Arrays.toString(message) + "]";
+	}
+	
+	
+
+}
