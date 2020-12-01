49dcfd1845a4912ee4b94bbb1532b55f93fb3c08
John Doe <johndoe@example.com>
2020-12-01 12:11:12 -0300

    refactor en entities

--- src/main/java/fuegoQuasar/quasar/model/entities/MensajeInterceptado.java
+++ src/main/java/fuegoQuasar/quasar/model/entities/MensajeInterceptado.java
@@ -67,16 +67,16 @@ public class MensajeInterceptado {
 		this.id = id;
 	}
 
-
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((distance == null) ? 0 : distance.hashCode());
-		result = prime * result + Arrays.hashCode(message);
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		result = prime * result + ((name == null) ? 0 : name.hashCode());
 		return result;
 	}
+
 
 	@Override
 	public boolean equals(Object obj) {
@@ -87,12 +87,10 @@ public class MensajeInterceptado {
 		if (getClass() != obj.getClass())
 			return false;
 		MensajeInterceptado other = (MensajeInterceptado) obj;
-		if (distance == null) {
-			if (other.distance != null)
+		if (id == null) {
+			if (other.id != null)
 				return false;
-		} else if (!distance.equals(other.distance))
-			return false;
-		if (!Arrays.equals(message, other.message))
+		} else if (!id.equals(other.id))
 			return false;
 		if (name == null) {
 			if (other.name != null)
@@ -101,8 +99,8 @@ public class MensajeInterceptado {
 			return false;
 		return true;
 	}
-	
-	
+
+
 	@Override
 	public String toString() {
 		return "MensajeInterceptado [name=" + name + ", distance=" + distance + ", message="
--- src/main/java/fuegoQuasar/quasar/model/entities/SateliteRebelde.java
+++ src/main/java/fuegoQuasar/quasar/model/entities/SateliteRebelde.java
@@ -61,16 +61,16 @@ public class SateliteRebelde {
 		this.coordenadaY = coordenadaY;
 	}
 
+
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((coordenadaX == null) ? 0 : coordenadaX.hashCode());
-		result = prime * result + ((coordenadaY == null) ? 0 : coordenadaY.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
 		return result;
 	}
+
 
 	@Override
 	public boolean equals(Object obj) {
@@ -81,28 +81,14 @@ public class SateliteRebelde {
 		if (getClass() != obj.getClass())
 			return false;
 		SateliteRebelde other = (SateliteRebelde) obj;
-		if (coordenadaX == null) {
-			if (other.coordenadaX != null)
-				return false;
-		} else if (!coordenadaX.equals(other.coordenadaX))
-			return false;
-		if (coordenadaY == null) {
-			if (other.coordenadaY != null)
-				return false;
-		} else if (!coordenadaY.equals(other.coordenadaY))
-			return false;
 		if (id == null) {
 			if (other.id != null)
 				return false;
 		} else if (!id.equals(other.id))
 			return false;
-		if (nombre == null) {
-			if (other.nombre != null)
-				return false;
-		} else if (!nombre.equals(other.nombre))
-			return false;
 		return true;
 	}
+
 
 	@Override
 	public String toString() {
